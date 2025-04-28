00d964740788e2618de71d180318cbd3e34d65a6
Advistane <Advistane@gmail.com>
2025-04-28 17:30:00 -0500

    Updated home page

--- client/src/components/HomeInfo.tsx
+++ client/src/components/HomeInfo.tsx
@@ -1,11 +1,37 @@
-﻿import ItemSearch from "./ItemSearch.tsx";
-
+﻿import {Button, Card, Group, Image, Text} from "@mantine/core";
+import {IconBrandGithub} from '@tabler/icons-react';
 export default function HomeInfo() {
 	return (
 		<div>
-			<h1>Home</h1>
-			<p>This is the home page.</p>
-			<ItemSearch />
+			<Card shadow="sm" p="lg">
+				<Card.Section>
+					<Image
+						src="/home.png"
+						height={640}
+					/>
+					<Text size="sm">The website is a work in progress...but you can use the Runelite plugin</Text>
+
+					<Group justify="center" mt="md" mb="xs">
+						<Button
+							component="a"
+							href="https://github.com/Advistane/clogged"
+							target ="_blank"
+						>
+							<IconBrandGithub size={32} stroke={1.5} color="black" />
+							Plugin Source
+						</Button>
+
+						<Button
+							component="a"
+							href="https://github.com/Advistane/clogged-server"
+							target ="_blank"
+						>
+							<IconBrandGithub size={32} stroke={1.5} color="black" />
+							Server Source
+						</Button>
+					</Group>
+				</Card.Section>
+			</Card>
 		</div>
 	)
 }
