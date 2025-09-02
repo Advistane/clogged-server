eb6d5a149423364457638432e0e92c98937ab0c4
Advistane <Advistane@gmail.com>
2025-09-02 18:42:41 -0500

    Added debug log

--- server/src/worker.ts
+++ server/src/worker.ts
@@ -159,6 +159,7 @@ const processClogUpdate = async (job: Job<UserCollectionData>) => {
 
 		} else {
 			log.info('No subcategory KCs provided in job data.');
+			log.debug(job.data)
 		}
 
 		await client.query('COMMIT');
